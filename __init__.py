30373e24ecd778dd91d25e69d0084f3beabcfae3
Bhavani Ravi <bhava0895@gmail.com>
2018-06-12 23:19:53 +0530

    add csv logger

--- __init__.py
+++ __init__.py
@@ -1,15 +1,26 @@
 import nltk
+import csv
+
 sent_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+def log_chat(text, pos_tags, return_dict):
+    with open('log.csv','a') as file:
+        writer = csv.writer(file)
+        writer.writerows([[text, str(pos_tags), str(return_dict)]])
 
 def get_entity_action(sentence):
     tokens = nltk.word_tokenize(sentence)
     word_pos = nltk.pos_tag(tokens)
     print ("\n\nPOS Tags ....")
     print (word_pos)
+
     nouns = [word for word,pos in word_pos if pos in ['NN','NNP','NNS','NNPS']]
     verbs = [word for word,pos in word_pos if pos in ['VB','VBD','VBG','VBN','VBP','VBZ']]
     question = [word for word,pos in word_pos if pos in ["WDT","WP","WP$","WRB"]]
-    return {"nouns":nouns, "verbs":verbs, "question_words": question}
+
+    return_dict =  {"nouns":nouns, "verbs":verbs, "question_words": question}
+    log_chat(sentence, word_pos, return_dict)
+    return return_dict
 
 
 text = input("Enter Text :: ")
